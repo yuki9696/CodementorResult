7c69b51a0245774268345d541c4bda2fb8518cd3
yuki9696 <yukiyamamoto@yuki-no-macbook-air.local>
2017-02-20 22:34:53 +0900

    yeah

--- audioDidFinish/ViewController.swift
+++ audioDidFinish/ViewController.swift
@@ -25,7 +25,6 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         for item in queue.items() {
             NotificationCenter.default.addObserver(self, selector: #selector(playerItemDidReachEnd(notification:)), name: .AVPlayerItemDidPlayToEndTime, object: item)
         }
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -35,20 +34,21 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     let music1 = Bundle.main.bundleURL.appendingPathComponent("music1.mp3")
     let music2 = Bundle.main.bundleURL.appendingPathComponent("music2.mp3")
     let music3 = Bundle.main.bundleURL.appendingPathComponent("music3.mp3")
-    let music4 = Bundle.main.bundleURL.appendingPathComponent("music3.mp3")
+    let music4 = Bundle.main.bundleURL.appendingPathComponent("music4.mp3")
 
     
     
     let url1 = Bundle.main.bundleURL.appendingPathComponent("music1.mp3")
     let url2 = Bundle.main.bundleURL.appendingPathComponent("music2.mp3")
     let url3 = Bundle.main.bundleURL.appendingPathComponent("music3.mp3")
-    let url4 = Bundle.main.bundleURL.appendingPathComponent("music3.mp3")
+    let url4 = Bundle.main.bundleURL.appendingPathComponent("music4.mp3")
 
 
-    
     @IBOutlet weak var yourButton1: customButton!
     @IBOutlet weak var yourButton2: customButton!
     @IBOutlet weak var yourButton3: customButton!
+    @IBOutlet weak var yourButton4: customButton!
+    
     
     
     fileprivate var player1:AVAudioPlayer?
@@ -57,33 +57,31 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
 
     
     
-    @IBAction func pushButton1(sender: UIButton) {
-        audioPlayer1(url: url1)
-    }
-    
-    @IBAction func pushButton2(sender: UIButton) {
-        audioPlayer2(url: url2)
-    }
-    
-    @IBAction func pushButton3(_ sender: UIButton) {
+    @IBAction func pushButton1(_ sender: UIButton) {
         //audioPlayer3(url: url1, url2: url2)
         sender.isSelected = true
         queue.play()
     }
     
-
+    @IBAction func pushButton2(sender: UIButton) {
+        audioPlayer1(url: url1)
+    }
     
+    @IBAction func pushButton3(sender: UIButton) {
+        audioPlayer2(url: url2)
+    }
+    
+    
+    @IBAction func pushButton4(_ sender: UIButton) {
+    }
+
 
     
         func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
             if (player === player1) {
-                yourButton1.isSelected = false
-            } else if (player === player2) {
                 yourButton2.isSelected = false
-            } else if (player === player3) {
+            } else if (player === player2) {
                 yourButton3.isSelected = false
-                player1!.play()
-                yourButton1.isSelected = true
             }
         }
     
@@ -91,21 +89,23 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     func playerItemDidReachEnd(notification: NSNotification) {
         
         if notification.object as? AVPlayerItem == items[0] {
-            yourButton3.isSelected = false
-            yourButton1.isSelected = true
-        }
-        
-        if notification.object as? AVPlayerItem == items[1] {
             yourButton1.isSelected = false
             yourButton2.isSelected = true
         }
         
-        if notification.object as? AVPlayerItem == items[2] {
+        if notification.object as? AVPlayerItem == items[1] {
             yourButton2.isSelected = false
+            yourButton3.isSelected = true
+        }
+        
+        if notification.object as? AVPlayerItem == items[2] {
+            yourButton3.isSelected = false
+            yourButton4.isSelected = true
             //yourButton1.isSelected = true
         }
         
         if notification.object as? AVPlayerItem == items[3] {
+            yourButton4.isSelected = false
             print("item 3")
         }
         
@@ -121,7 +121,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         do {
             try player1 = AVAudioPlayer(contentsOf:url)
             player1!.play()
-            yourButton1.isSelected = true
+            yourButton2.isSelected = true
             player1!.delegate = self
         } catch {
             print(error)
@@ -132,25 +132,15 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         do {
             try player2 = AVAudioPlayer(contentsOf:url)
             player2!.play()
-            yourButton2.isSelected = true
+            yourButton3.isSelected = true
             player2!.delegate = self
             
         } catch {
             print(error)
         }
     }
-    func audioPlayer3(url: URL, url2: URL) {
-        do {
-            try player3 = AVAudioPlayer(contentsOf:url3)
-            try player1 = AVAudioPlayer(contentsOf: url1)
-            player3!.play()
-            yourButton3.isSelected = true
-            player3!.delegate = self
-            player1!.delegate = self
-        } catch {
-            print(error)
-        }
-    }
-
 
 }
+
+
+
